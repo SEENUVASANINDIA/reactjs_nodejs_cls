39e7d8b00928b28a192f662e92560438bd552544
Seenuvasan <seenuvasanindiaa>
2023-04-08 18:48:27 +0530

    26-2-23

---
+++ 26-2-23/logmatui/src/App.js
@@ -0,0 +1,23 @@
+// import logo from './logo.svg';
+import './App.css';
+// import { Box } from '@mui/system';
+import { Box } from '@mui/material';
+
+function App() {
+  return (
+  <>
+  <Box sh ={{}}>
+    test
+  </Box>
+  <Box sx={{display:"flex",minHeight:"100vh",justifyContent:"center",backgroundColor:"white",textDecoration:{color:"red"}}}>
+fex Box
+<Box>
+
+</Box>
+  </Box>
+  <Box></Box>
+  </>
+  );
+}
+
+export default App;
